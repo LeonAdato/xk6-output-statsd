9b28742bb9d93ee76938aace47e4b232f79f8ad2
Paul Balogh <javaducky@gmail.com>
2023-07-31 10:27:42 -0500

    Add implementation from core k6
    Implementation is a snapshot of the k6 output/statsd package as of v0.45.0@718478619d582dadd6b1971f81c0daf15d461fa7.
    
    Signed-off-by: Paul Balogh <javaducky@gmail.com>

--- go.mod
+++ go.mod
@@ -3,9 +3,12 @@ module github.com/javaducky/xk6-output-statsd
 go 1.19
 
 require (
+	github.com/DataDog/datadog-go v0.0.0-20180330214955-e67964b4021a
+	github.com/mstoykov/envconfig v1.4.1-0.20220114105314-765c6d8c76f1
 	github.com/sirupsen/logrus v1.9.0
 	github.com/stretchr/testify v1.8.2
 	go.k6.io/k6 v0.45.0
+	gopkg.in/guregu/null.v3 v3.5.0
 )
 
 require (
@@ -23,6 +26,5 @@ require (
 	golang.org/x/text v0.9.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
-	gopkg.in/guregu/null.v3 v3.5.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- pkg/statsd/output.go
+++ pkg/statsd/output.go
@@ -1,59 +1,117 @@
-// Package statsd performs output operations for the extension
 package statsd
 
 import (
+	"fmt"
 	"time"
 
+	"github.com/DataDog/datadog-go/statsd"
 	"github.com/sirupsen/logrus"
 
+	"go.k6.io/k6/metrics"
 	"go.k6.io/k6/output"
 )
 
-// Output implements the lib.Output interface
-type Output struct {
-	output.SampleBuffer
-
-	config          Config
-	periodicFlusher *output.PeriodicFlusher
-	logger          logrus.FieldLogger
+// New creates a new statsd connector client
+func New(params output.Params) (output.Output, error) {
+	return newOutput(params)
 }
 
-var _ output.Output = new(Output)
-
-// New creates an instance of the collector
-func New(p output.Params) (*Output, error) {
-	conf, err := NewConfig(p)
+func newOutput(params output.Params) (*Output, error) {
+	conf, err := getConsolidatedConfig(params.JSONConfig, params.Environment, params.ConfigArgument)
 	if err != nil {
 		return nil, err
 	}
-	// Some setupping code
+	logger := params.Logger.WithFields(logrus.Fields{"output": "statsd"})
 
 	return &Output{
 		config: conf,
-		logger: p.Logger,
+		logger: logger,
 	}, nil
 }
 
-// Description returns a human-readable description of the output that will be shown in `k6 run`
+var _ output.Output = &Output{}
+
+// Output sends result data to statsd daemons with the ability to send to datadog as well
+type Output struct {
+	output.SampleBuffer
+
+	periodicFlusher *output.PeriodicFlusher
+
+	config config
+
+	logger logrus.FieldLogger
+	client *statsd.Client
+}
+
+func (o *Output) dispatch(entry metrics.Sample) error {
+	var tagList []string
+	if o.config.EnableTags.Bool {
+		tagList = processTags(o.config.TagBlocklist, entry.Tags.Map())
+	}
+
+	switch entry.Metric.Type {
+	case metrics.Counter:
+		return o.client.Count(entry.Metric.Name, int64(entry.Value), tagList, 1)
+	case metrics.Trend:
+		return o.client.TimeInMilliseconds(entry.Metric.Name, entry.Value, tagList, 1)
+	case metrics.Gauge:
+		return o.client.Gauge(entry.Metric.Name, entry.Value, tagList, 1)
+	case metrics.Rate:
+		if check, ok := entry.Tags.Get("check"); ok {
+			return o.client.Count(
+				checkToString(check, entry.Value),
+				1,
+				tagList,
+				1,
+			)
+		}
+		return o.client.Count(entry.Metric.Name, int64(entry.Value), tagList, 1)
+	default:
+		return fmt.Errorf("unsupported metric type %s", entry.Metric.Type)
+	}
+}
+
+func checkToString(check string, value float64) string {
+	label := "pass"
+	if value == 0 {
+		label = "fail"
+	}
+	return "check." + check + "." + label
+}
+
+// Description returns a human-readable description of the output.
 func (o *Output) Description() string {
-	return "statsd: " + o.config.Address
+	return fmt.Sprintf("statsd (%s)", o.config.Addr.String)
 }
 
-// Stop flushes all remaining metrics and finalizes the test run
-func (o *Output) Stop() error {
-	o.logger.Debug("Stopping...")
-	defer o.logger.Debug("Stopped!")
-	o.periodicFlusher.Stop()
-	return nil
-}
-
-// Start performs initialization tasks prior to Engine using the output
+// Start tries to open a connection to specified statsd service and starts the goroutine for
+// metric flushing.
 func (o *Output) Start() error {
 	o.logger.Debug("Starting...")
 
-	// Here we should connect to a service, open a file or w/e else we decided we need to do
+	var err error
+	if address := o.config.Addr.String; address == "" {
+		err = fmt.Errorf(
+			"connection string is invalid. Received: \"%+s\"",
+			address,
+		)
+		o.logger.Error(err)
 
-	pf, err := output.NewPeriodicFlusher(o.config.PushInterval, o.flushMetrics)
+		return err
+	}
+
+	o.client, err = statsd.NewBuffered(o.config.Addr.String, int(o.config.BufferSize.Int64))
+
+	if err != nil {
+		o.logger.Errorf("Couldn't make buffered client, %s", err)
+		return err
+	}
+
+	if namespace := o.config.Namespace.String; namespace != "" {
+		o.client.Namespace = namespace
+	}
+
+	pf, err := output.NewPeriodicFlusher(o.config.PushInterval.TimeDuration(), o.flushMetrics)
 	if err != nil {
 		return err
 	}
@@ -63,20 +121,45 @@ func (o *Output) Start() error {
 	return nil
 }
 
+// Stop flushes any remaining metrics and stops the goroutine.
+func (o *Output) Stop() error {
+	o.logger.Debug("Stopping...")
+	defer o.logger.Debug("Stopped!")
+	o.periodicFlusher.Stop()
+	return o.client.Close()
+}
+
 func (o *Output) flushMetrics() {
 	samples := o.GetBufferedSamples()
 	start := time.Now()
 	var count int
+	var errorCount int
 	for _, sc := range samples {
 		samples := sc.GetSamples()
 		count += len(samples)
-		for _, sample := range samples {
-			// Here we actually write or accumulate to then write in batches
-			// for the statsd code we just ... dump some parts of it on the screen
-			o.logger.Infof("%s=%.5f,%s\n", sample.Metric.Name, sample.Value, sample.GetTags().Map())
+		o.logger.
+			WithField("samples", len(samples)).
+			Debug("Pushing metrics to server")
+
+		for _, entry := range samples {
+			if err := o.dispatch(entry); err != nil {
+				// No need to return error if just one metric didn't go through
+				o.logger.WithError(err).Debugf("Error while sending metric %s", entry.Metric.Name)
+				errorCount++
+			}
 		}
 	}
+
 	if count > 0 {
-		o.logger.WithField("t", time.Since(start)).WithField("count", count).Debug("Wrote metrics to stdout")
+		if errorCount != 0 {
+			o.logger.Warnf("Couldn't send %d out of %d metrics. Enable verbose logging with --verbose to see individual errors",
+				errorCount, count)
+		}
+		if err := o.client.Flush(); err != nil {
+			o.logger.
+				WithError(err).
+				Error("Couldn't flush a batch")
+		}
+		o.logger.WithField("t", time.Since(start)).WithField("count", count).Debug("Wrote metrics to statsd")
 	}
 }
